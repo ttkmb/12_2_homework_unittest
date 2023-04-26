a3cfc9ad50d2bce3e48f16c26964fbf0c3f0e790
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2023-04-26 16:06:08 +0300

    edit tests to 100% coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,8 +8,11 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         with self.assertRaises(IndexError):
             self.assertEqual(arrs.get([], 0, "test"), "test")
-
+        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1, 2), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4,), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1,), [3])
